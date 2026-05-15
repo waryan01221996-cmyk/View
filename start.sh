dd7f811b3f6ced0b83ce7d4f9d986b6ca901ed9c
waryan01221996-cmyk <waryan01221996@gmail.com>
2026-05-15 19:03:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-# Jalankan Tor di background
-echo "Starting Tor..."
-sudo service tor start
-
-# Jalankan Privoxy di background
-echo "Starting Privoxy..."
-sudo service privoxy start
-
-# Tunggu agar Tor membangun sirkuit (IP baru)
-echo "Waiting for Tor to stabilize..."
-sleep 20
-
-# Jalankan bot Python kamu
-echo "Launching the bot..."
+echo "Menunggu sirkuit Tor..."
+sleep 25
 python bot.py
